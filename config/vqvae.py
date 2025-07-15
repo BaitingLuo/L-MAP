004f1e0d203dc115d3983ac701340e3026edd031
baiting <luobaiting518918@outlook.com>
2025-07-14 19:14:48 -0500

    Update config

--- config/vqvae.py
+++ config/vqvae.py
@@ -25,8 +25,8 @@ base = {
         'n_layer': 4,
         'n_head': 4,
         ## number of epochs for a 1M-size dataset; n_epochs = 1M / dataset_size * n_epochs_ref
-        'n_epochs_ref': 1,
-        'n_saves': 1,
+        'n_epochs_ref': 50,
+        'n_saves': 3,
         'logbase': logbase,
         'device': 'cuda',
 
